ca272e6a222c4ff79bef7e1d7021c07061465ded
deepakantony <deepakantony@gmail.com>
2011-03-30 17:52:35 -0700

    few functions declared

--- da_ds/bigint.cpp
+++ da_ds/bigint.cpp
@@ -4,8 +4,32 @@
 
 using namespace std;
 
+string convert_base(string n, int from_b, int to_b) {
+}
+
 class BigInt {
+public:
+  BigInt():_data(string("0")) {}
+  BigInt(const BigInt &n):_data(n._data) {}
+  BigInt(string str_n, int base = 10);
+  
+  BigInt& operator+=(const BigInt &n);
+  const BigInt operator+(const BigInt &n) const { return BigInt(*this)+=n; }
+  
+  BigInt& operator*=(const BigInt &n);
+  const BigInt operator*(const BigInt &n) const { return BigInt(*this)*=n; }
+private:
+  string _data;
 };
+
+BigInt::BigInt(string str_n, int base = 10) {
+  
+}
+
+BigInt& operator+=(const BigInt &n) {
+  
+  return *this;
+}
 
 int main(int argc, char **argv) {
 }
